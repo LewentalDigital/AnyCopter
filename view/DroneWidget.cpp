320c527533cfda61a85cf706fc4f264970050569
Lewental Digital <lewental.s@gmail.com>
2024-02-02 18:58:21 +0100

    Implementata immagini droni non pixellate, closes #2

--- view/DroneWidget.cpp
+++ view/DroneWidget.cpp
@@ -8,8 +8,7 @@ DroneWidget::DroneWidget(Drone* d, QWidget* parent) : QWidget(parent), drone(d) 
     QHBoxLayout* layout = new QHBoxLayout(this);
     layout->setAlignment(Qt::AlignLeft | Qt::AlignTop);
 
-    image = new QLabel();
-    image->setPixmap(QPixmap(":assets/images/agriDrone.png").scaledToHeight(200));
+    image->setPixmap(QPixmap(":assets/images/agriDrone.png").scaled(200, 300,Qt::KeepAspectRatio, Qt::SmoothTransformation));
     name = new QLabel(QString::fromStdString(drone->getName()));
     batteryLevel = new QLabel(QString::number(drone->getBatteryLevel()) + "%");
     numSensors = new QLabel(QString::number(drone->getNumEquippedSensors()) + " sensors equipped");
