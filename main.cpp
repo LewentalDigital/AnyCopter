a2630ea278316420892772335c22438986fb88fc
Lewental Digital <lewental.s@gmail.com>
2024-02-05 21:34:58 +0100

    Impostazione icone del programma candidate, da finire il design

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    // app.setWindowIcon(QIcon(":assets/icon.svg"));
+    app.setWindowIcon(QIcon(":assets/icons/icon.svg"));
+    app.setWindowIcon(QIcon(":assets/icons/candidate.png"));
 
     // QFile style(":/assets/style.qss");
     // style.open(QFile::ReadOnly);
